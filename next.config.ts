b6b70bb383c7879b60f437e2fd6a4f810b10f333
sorrellp <psorrell1@gmail.com>
2026-02-16 22:09:06 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  serverExternalPackages: ["@copilotkit/runtime"],
+};
 
 export default nextConfig;
